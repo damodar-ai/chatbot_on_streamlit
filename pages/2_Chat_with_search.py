e933942597a85becbac629268c9598288817b8af
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-12-03 23:01:16 +0530

    Update 2_Chat_with_search.py

--- pages/2_Chat_with_search.py
+++ pages/2_Chat_with_search.py
@@ -10,11 +10,7 @@ with st.sidebar:
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-    # openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
-    # openai_api_key = "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"
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/Damodar12345678/chatbot_on_streamlit/blob/main/pages/2_Chat_with_search.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
 
 st.title("🔎 सर्वज्ञ - चाहे जो पूछ लो")
 
