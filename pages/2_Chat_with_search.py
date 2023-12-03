6ebad6c5118e86732aec250b620119f435ce3cee
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-12-03 20:43:10 +0530

    Update 2_Chat_with_search.py

--- pages/2_Chat_with_search.py
+++ pages/2_Chat_with_search.py
@@ -26,7 +26,7 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
+if prompt := st.chat_input(placeholder="Who won the election in MP?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
