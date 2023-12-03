845679fa88bb028dc477c05095147073071bc8df
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-12-03 22:06:46 +0530

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -55,4 +55,4 @@ def test_Langchain_Quickstart(langchain_llm):
     at.button[0].set_value(True).run()
     print(at)
     assert at.info[0].value == RESPONSE
-st.title("Flowers Classifier")
+st.title("🔎 LangChain - Chat with search")
