dd274f2205ecb6c20ecf00ee47ad5881af7a2243
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-12-05 21:54:43 +0530

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-st.image('https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80', caption='Sunrise by the mountains')
+# st.image('https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80', caption='Sunrise by the mountains')
 st.markdown(
         """
         <style>
@@ -13,3 +13,4 @@ st.markdown("""
         <h2 style="color:Black;text-align:center;">Loan Approval Prediction App </h2>
     </div>
     """, unsafe_allow_html=True)
+st.image('https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80', caption='Sunrise by the mountains')
