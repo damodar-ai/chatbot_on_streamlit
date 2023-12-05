b50db0ee05a972cb4b641acf52026af3f19ce84a
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-12-05 21:50:57 +0530

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,1 +1,3 @@
 
+import streamlit as st
+st.image('https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80', caption='Sunrise by the mountains')
