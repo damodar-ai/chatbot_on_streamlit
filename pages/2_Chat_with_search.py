a17d6c36a22fd33299690494c588466c44858892
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-12-07 18:26:07 +0530

    Update 2_Chat_with_search.py

--- pages/2_Chat_with_search.py
+++ pages/2_Chat_with_search.py
@@ -1,51 +1,26 @@
-import streamlit as st
-
-from langchain.agents import initialize_agent, AgentType
-from langchain.callbacks import StreamlitCallbackHandler
-from langchain.chat_models import ChatOpenAI
-from langchain.tools import DuckDuckGoSearchRun
-
-
+First
+import openai import streamlit as st
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", type="password")
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/Damodar12345678/chatbot_on_streamlit?quickstart=1)"
-    # "[st.write["Try It Out With This API Ket :- "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""]]"
-    '''Try It Out With This API Ket :- `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`'''
-# " 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"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("🔎 Sarvagya - Ask Whatever You Want")
-
-"""
-
-This application is `capable of answering all your questions` based
-on the input given by your files, chats and your prompts.  Its name
-is Sarvagya. You can use it by entering the API key of your Open AI. 🤝  
-[github.com/langchain-ai/streamlit-agent](https://github.com/Damodar12345678/chatbot_on_streamlit).
-"""
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "assistant", "content": "Hi, I'm a chatbot who can search the web. How can I help you?"}
-    ]
+st.title("💬 Chatbot") if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input(placeholder="Who won the election in MP?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-
+if prompt := st.chat_input():
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, streaming=True)
-    search = DuckDuckGoSearchRun(name="Search")
-    search_agent = initialize_agent([search], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handle_parsing_errors=True)
-    with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-        response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        st.write(response)
-
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
