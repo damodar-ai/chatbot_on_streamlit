758f223dc3b9ad3c81e6756021ae69f62be11da7
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-12-07 20:32:39 +0530

    Update 2_Chat_with_search.py

--- pages/2_Chat_with_search.py
+++ pages/2_Chat_with_search.py
@@ -1,6 +1,5 @@
-First
-import openai 
-import streamlit as st
+# First
+import openai import streamlit as st
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
